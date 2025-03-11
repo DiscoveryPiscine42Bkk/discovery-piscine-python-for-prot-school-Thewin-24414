84eee95bfe981cc7f0fd1c21ea915ccc5592c6ab
Thewin-24414 <40763@prot.ac.th>
2025-03-11 12:22:17 +0700

    ex02

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+x = str(input("What you gotta say? :"))
+i = str(input("I got that! Anything else? :"))
+
+while i != "STOP":
+   i = str(input("I got that! Anything else? :"))
+   if i == "STOP":
+     break
