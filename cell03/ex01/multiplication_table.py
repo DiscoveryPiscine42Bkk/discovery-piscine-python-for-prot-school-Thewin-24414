be8029dcd08cfe0803cf72dff8b8fa77e48657d2
Thewin-24414 <40763@prot.ac.th>
2025-03-11 12:00:32 +0700

    Update multiplication_table.py

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -2,3 +2,11 @@ x = int(input("Enter a number :"))
 n = 10
 for i in range(0,n):
 	print(f"{i} x {x} = {i*x}")
+
+# x = str(input("What you gotta say? :"))
+# i = str(input("I got that! Anything else? :"))
+
+# while i != "STOP":
+#    i = str(input("I got that! Anything else? :"))
+#    if i == "STOP":
+#      break
